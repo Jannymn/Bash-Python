7389ebf94fe2b8ab6cfbcd5dc804a2c0d51759b4
kol <1740024005@qq.com>
2017-11-13 15:36:22 +0800

    123

--- Application/zabbix_send.py
+++ Application/zabbix_send.py
@@ -9,12 +9,16 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 print "脚本名称:", sys.argv[0]
 def problemMessage():
-  return "##### <font color=red> Monitor message \nProblem </font>"
+  return "##### <font color=red> Monitor message \nProblem\n </font>"
+
 def resolvedMessage():
   return "##### <font color=green> Monitor message \nResolved </font>"
 
+def errorMessage():
+#  return (sys.argv[1])
+  return "##### <font color=red> %s </font>" % sys.argv[1]
 def extractionMessage():
-  return problemMessage() + '\n' + resolvedMessage()
+  return problemMessage() + '\n' + errorMessage()
 
 def sendDingDingMessage(url, data):
   req = urllib2.Request(url)
@@ -22,8 +26,10 @@ def sendDingDingMessage(url, data):
   opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
   response = opener.open(req, json.dumps(data))
   return response.read()
+
 def main():
   posturl = "https://oapi.dingtalk.com/robot/send?access_token=38ea4a51e74603e0b369ac2c74d831355c3a181b52b40ffa7286abcb63b121a5"
-  data = {"msgtype": "markdown", "markdown": {"text": extractionMessage(),"at": {"atMobiles": ["15622361127"], "isAtAll": "false"}}}
+  data = {"msgtype": "markdown", "markdown": {"text": extractionMessage(),"title": "Monitor Message"}, "at": {"atMobiles": "15622361127", "isAtAll": "false"}}
   sendDingDingMessage(posturl, data)
+
 main()
